19a1cd88b71e0e05c2d753bce3768428ba88e71d
fabricio.hemominas <fabricio.hemominas@gmail.com>
2015-04-17 17:59:03 +0000

    Projeto HemoProject no Eclipse -

---
+++ workspace/HemoProject/src/br/org/mg/hemoproject/controle/FileUploadView.java
@@ -0,0 +1,53 @@
+package br.org.mg.hemoproject.controle;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
+
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.UploadedFile;
+
+@ManagedBean
+public class FileUploadView {
+
+    private UploadedFile file;
+
+    public UploadedFile getFile() {
+        return file;
+    }
+
+    public void setFile(UploadedFile file) {
+        this.file = file;
+    }
+
+    public void upload(FileUploadEvent event) {
+        file = event.getFile();
+
+        if (file != null) {
+
+            File file1 = new File("C:\\Users\\12546446\\Desktop\\Projetos Fabricio\\Geliv\\build\\web\\fotos\\", file.getFileName());
+            try {
+                FileOutputStream fos = new FileOutputStream(file1);
+                fos.write(event.getFile().getContents());
+                fos.close();
+
+                FacesContext instance = FacesContext.getCurrentInstance();
+                instance.addMessage("mensagens", new FacesMessage(
+                        FacesMessage.SEVERITY_ERROR,
+                        file.getFileName() + " anexado com sucesso", null));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ workspace/HemoProject/src/br/org/mg/hemoproject/controle/UsuarioBean.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.org.mg.hemoproject.controle;
+
+import javax.faces.bean.ManagedBean;
+
+import br.org.mg.hemoproject.conecta.Login;
+
+@ManagedBean
+public class UsuarioBean {
+
+    private String masp;
+    private String senha;
+    private String nome;
+
+    public String getLogin(String usuario, String senha) {
+        String pag = "erro";
+        Login login = new Login();
+        String displayName = login.efetuaLogin(usuario, senha);
+
+        if (!displayName.equals("null")) {
+            pag = "home";
+            this.nome = displayName;
+        }
+
+        MessagesView ms = new MessagesView();
+        ms.info(nome, "Bem vindo!");
+        
+        return pag;
+    }
+
+    public void getDisplayName(String usuario) {
+        
+        /*
+         if(nome != null){
+         return "Bem vindo " + nome;
+         }
+        
+         return "";
+         */
+    }
+
+    public String getMasp() {
+        return masp;
+    }
+
+    public void setMasp(String masp) {
+        this.masp = masp;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
