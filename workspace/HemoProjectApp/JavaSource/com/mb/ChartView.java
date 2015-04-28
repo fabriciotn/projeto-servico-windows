7ef8db21451a3282ea70b5b354c1479b49d04c53
fabricio.hemominas <fabricio.hemominas@gmail.com>
2015-04-28 11:49:56 +0000

    Mudança para o Oracle. - Verificar sequencias! - Verificar o chartview

--- workspace/HemoProjectApp/JavaSource/com/mb/ChartView.java
+++ workspace/HemoProjectApp/JavaSource/com/mb/ChartView.java
@@ -13,58 +13,56 @@ import org.primefaces.model.chart.ChartSeries;
 import org.primefaces.model.chart.LegendPlacement;
 
 import com.facade.PendenciaFacade;
- 
+
 @ManagedBean
 public class ChartView implements Serializable {
- 
-    private BarChartModel animatedModel2;
- 
-    @PostConstruct
-    public void init() {
-        createAnimatedModels();
-    }
- 
-    public BarChartModel getAnimatedModel2() {
-        return animatedModel2;
-    }
- 
-    private void createAnimatedModels() {
-        animatedModel2 = initBarModel();
-        animatedModel2.setTitle("Pendências por setor");
-        animatedModel2.setAnimate(true);
-        animatedModel2.setLegendPosition("su");
-        animatedModel2.setLegendPlacement(LegendPlacement.OUTSIDEGRID);
-        animatedModel2.setShowPointLabels(true);
-        
-        Axis yAxis = animatedModel2.getAxis(AxisType.Y);
-        yAxis.setLabel("Quantidade de pendências");
-        
-    }
-     
-    private BarChartModel initBarModel() {
-    	String sql = "select p.id as id, "
-				+ "p.titulo as titulo, "
-				+ "p.status as status, "
-				+ "s.nome as setor, "
-				+ "count(*) as qtd_Pendencia "
-				+ "from Pendencia p, "
-				+ "Setor s where p.setor = s.id "
-				+ "group by s";
-		
+
+	private BarChartModel animatedModel2;
+
+	@PostConstruct
+	public void init() {
+		createAnimatedModels();
+	}
+
+	public BarChartModel getAnimatedModel2() {
+		return animatedModel2;
+	}
+
+	private void createAnimatedModels() {
+		animatedModel2 = initBarModel();
+		animatedModel2.setTitle("Pendências por setor");
+		animatedModel2.setAnimate(true);
+		animatedModel2.setLegendPosition("su");
+		animatedModel2.setLegendPlacement(LegendPlacement.OUTSIDEGRID);
+		animatedModel2.setShowPointLabels(true);
+
+		Axis yAxis = animatedModel2.getAxis(AxisType.Y);
+		yAxis.setLabel("Quantidade de pendências");
+
+	}
+
+	private BarChartModel initBarModel() {
+		String sql = "SELECT p.id AS id, "
+				+ "p.titulo AS titulo, "
+				+ "p.status AS status, "
+				+ "setor.nome AS setor, "
+				+ "COUNT(*) AS qtd_Pendencia "
+				+ "FROM Pendencia p, SETOR setor "
+				+ "WHERE p.setor_id = setor.id "
+				+ "GROUP BY setor.nome, p.id, p.titulo, p.status";
+
 		PendenciaFacade facade = new PendenciaFacade();
 		List<Object[]> lista = facade.buscaComQuery(sql);
-		
+
 		BarChartModel model = new BarChartModel();
 		ChartSeries setor;
 		for (Object[] objects : lista) {
 			setor = new ChartSeries(objects[3].toString());
-	        setor.set("Setores",(Number) objects[4]);
-	        model.addSeries(setor);
+			setor.set("Setores", (Number) objects[4]);
+			model.addSeries(setor);
 		}
-    	
-        return model;
-    }
-     
-    
-    
+
+		return model;
+	}
+
 }
--- workspace/HemoProjectApp/JavaSource/com/model/Iteracao.java
+++ workspace/HemoProjectApp/JavaSource/com/model/Iteracao.java
@@ -18,7 +18,6 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 @Entity
-//@Table(name="PR_ITERACAO") 
 public class Iteracao implements Serializable{
 
 	private static final long serialVersionUID = 1L;
